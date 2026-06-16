f97ac2eedb1ad2cd1e513fa5f16e3cb1d9d59e4b
fbacalini90 <fbacalini90@gmail.com>
2026-06-16 14:12:56 -0300

    Add files via upload

---
+++ src/enums/Estado.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package enums;
+
+//ENUM Estado
+//Representa los posibles estados de un pedido
+public enum Estado {
+    PENDIENTE,
+    CONFIRMADO,
+    TERMINADO,
+    CANCELADO
+}
---
+++ src/interfaces/Calculable.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+//INTERFAZ Calculable
+//Calcula los totales de los pedidos (será implementada dentro de la clase Pedido)
+public interface Calculable {
+    double calcularTotal(); //Método abstracto para calcular el total de un pedido
+}
